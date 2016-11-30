046ee0d3be9271305819436af43b8aa67c6d7c95
zibi-mint <zbigniew.pytel@gmail.com>
2016-11-29 21:08:39 -0500

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+
+
+
+import { AppComponent } from './containers/app/app.component';
+
+import { FindUserPageComponent } from './containers/find-user-page/find-user-page-component';
+
+import { ViewBookPageComponent } from './containers/collection-page-component/collection-page-component';
+import { CollectionPageComponent } from './containers/collection-page-component/collection-page-component';
+import { NotFoundPageComponent } from './containers/collection-page-component/collection-page-component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
