7b3b23d529628a64220f2d01629e0d7f9d383f82
zpytel <zbigniew.pytel@sickkids.ca>
2016-12-08 15:39:49 -0500

    Working on Forms adding validation

--- src/app/components/add-car-form/add-car-form.component.ts
+++ src/app/components/add-car-form/add-car-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder,ValidatorFn, FormControl, FormGroup,FormGroupName, Validators } from '@angular/forms';
+
+function passwordMatcher(c:AbstractControl){
+  return c.get('pass').value===c.get('confirm').value?null:{'nomatch':true}
+}
 
 @Component({
   selector: 'app-add-car-form',
@@ -7,9 +11,21 @@ import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Valida
   styleUrls: ['./add-car-form.component.css']
 })
 export class AddCarFormComponent implements OnInit {
+  form:FormGroup;
+  constructor(public fb:FormBuilder) { 
+   this.form=this.fb.group({
+    mark:'',
+    model:'',
+    account:this.fb.group({
+      user:['',Validators.required],
+      pass:['',Validators.required],
+      confirm:['',Validators.required]
+    },{validator:passwordMatcher}),
+   });
+   this.form.patchValue({mark:"Ferrari",model:"Diablo"});
 
-  constructor() { }
-
+  }
+    
   ngOnInit() {
   }
 
