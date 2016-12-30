9527ca31bb04561382e195d3bea5b61e03460a8f
zpytel <zbigniew.pytel@sickkids.ca>
2016-12-30 11:52:54 -0500

    Elastic search

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -7,7 +7,7 @@ import {AngularFire} from 'angularfire2';
 
 
 import {Car} from '../models/cars'
-
+const PATH="search";
 @Injectable()
 export class CarService{
     
@@ -20,12 +20,26 @@ export class CarService{
     }
     searchCars(search:string):Observable<Car[]>{
      console.log("in serchcars()")
-     return this.fb.database.list('/cars');
+     return this.sarch("firebase","car",search+"*")
+     //return this.fb.database.list('/cars');
      //.map(items=>items.filter((item:Car)=>item.mark.indexOf(search)));
     
     }
     addCar(car:Car):any{
        return this.fb.database.object('/cars').set(car).then(()=>car);
         
+
     }
+
+    private sarch(index:string,type:string,searchTerm:string):Observable<any>{
+        var reqRef=this.fb.database.list(PATH+"/request");
+        var keyval=reqRef.push({index:index,type:type,q:searchTerm}).key;
+        return this.fb.database.list(PATH+"/response/"+ keyval).map(hit=>{
+            hit.reduce((pre:Car[],curr:any)=>{
+              return Object.assign(pre,{id:-1,make:curr._source.make,model:curr._source.model})
+            },[])
+        })
+      
+    }
+   
 }
